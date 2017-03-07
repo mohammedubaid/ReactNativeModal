ab0323286dac89a49c06c728b6317782aba4f681
mohammedubaid <mohammedubaidc@gmail.com>
2017-03-08 01:33:47 +0530

    changed android js

--- index.android.js
+++ index.android.js
@@ -3,8 +3,22 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+import React, { Component } from 'React'
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './src/reducers';
 import App from './src/app'
 
+class Main extends Component{
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <App />
+      </Provider>
+    );
+  }
+}
 
-AppRegistry.registerComponent('modal_popup', () => App);
+AppRegistry.registerComponent('modal_popup', () => Main);
